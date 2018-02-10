22c078b3a482b6020dfe648140055a72da8eac82
kalasni <kalassni@gmail.com>
2018-02-10 18:24:53 +0100

    Created a RSS feed with the file feeds.py

--- blog/models.py
+++ blog/models.py
@@ -11,15 +11,15 @@ from taggit.managers import TaggableManager
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset() \
-                                    .filter(status='published')
+            .filter(status='published')
 
 
 class Post(models.Model):
 
     STATUS_CHOICES = (
-            ('draft', 'Draft'),
-            ('published', 'Published'),
-        )
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
 
@@ -35,9 +35,9 @@ class Post(models.Model):
     # auto_now - the date will be updated automatically when creating an object
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES,
-        default='draft')
+                              default='draft')
 
-    objects = models.Manager() # The default manager
+    objects = models.Manager()  # The default manager
 
     # Custom manager. i.e, we can retrieve all published post whose title
     # starts with 'who' using:
@@ -56,13 +56,10 @@ class Post(models.Model):
     # It returns the canonical URL of the object
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                                    args=[self.publish.year,
-                                                self.publish.strftime('%m'),
-                                                self.publish.strftime('%d'),
-                                                self.slug])
-
-
-
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
 
 
 class Comment(models.Model):
@@ -87,5 +84,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return 'Comment by {} on {}'.format(self.name, self.post)
-
-
--- minisite/settings.py
+++ minisite/settings.py
@@ -133,8 +133,8 @@ STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'pematorrrs@gmail.com'
-EMAIL_HOST_PASSWORD = 'b3gir4gma'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
